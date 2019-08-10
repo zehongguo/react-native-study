7195fb301fdff8f9a8b2b859048358ba6d1a88b2
MrsGuo <972134688@qq.com>
2019-08-10 15:13:36 +0800

    init app

---
+++ index.ios.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class MyApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.nav}>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '',
+  },
+});
+
+AppRegistry.registerComponent('MyApp', () => MyApp);
